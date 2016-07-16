49a30368b2fd1f4032dbc01a8925085bfa16b7b8
LALITHASHREE <dlalithashree@gmail.com>
2016-07-16 15:06:52 +0530

    finish

---
+++ README.c
@@ -0,0 +1,13 @@
+# reverse-a-string1
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+int rev;
+char mark[50];
+printf("enter the string");
+gets(mark);
+rev=strrev(mark);
+printf("reverse order  is,mark");
+ return 0;
+ }
